e5e76d60b3e566c7fccd5094339293af676852ec
Diego Suarez-2022233 <dsuarez-2022233@kinal.edu.gt>
2024-10-02 13:27:14 -0600

    ultimas actualizaciones de diseño

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavBarComp } from '../../components/navbars/Navbar';
 import { HeaderComp } from '../../components/navbars/Header';
-import './homeStyle.css'
+import './homeStyle.css';
 import { Footer } from '../../components/complements/Footer';
 
 export const Home = () => {
@@ -10,22 +10,17 @@ export const Home = () => {
             <HeaderComp />
             <NavBarComp />
             <div className="main-content">
-
                 <div className="welcome-message">
-
                     <img src="/src/assets/img/techlogix.png" alt="TechLogix Logo" />
                     <h1>Bienvenido!</h1>
                     <h2>TECHLOGIX</h2>
                     <p>Data Security & Technology</p>
                     <p>En un mundo donde la información se ha convertido en uno de los activos más valiosos</p>
-                    <p>ECHLOGIX se dedica a ofrecer soluciones avanzadas en seguridad de datos y tecnología</p>
-                    <p>Nuestro objetivo es proteger su información</p>
-                    <p>Garantizar la confidencialidad de sus datos y ofrecer tecnologías innovadoras que impulsen su negocio hacia el futuro.</p>
-
+                    <p>TECHLOGIX se dedica a ofrecer soluciones avanzadas en seguridad de datos y tecnología</p>
+                    <p>Nuestro objetivo es proteger su información, garantizar la confidencialidad de sus datos y ofrecer tecnologías innovadoras que impulsen su negocio hacia el futuro.</p>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 };
-
